a24d3ac26d59d4d0572ce6cfa5bfb6eab19df3be
deepaksnath <deepak.s.nath@gmail.com>
2025-02-19 12:24:01 +0530

    Unit test cases added.

---
+++ test/EasyGrocery.UnitTest/Application/Handlers/CustomerHandlers/GetCustomerByIdQueryHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using EasyGrocery.Application.Handlers.CustomerHandler.Queries;
+using EasyGrocery.Domain.Entities;
+using EasyGrocery.Domain.Repositories;
+using FluentAssertions;
+using Moq;
+
+namespace EasyGrocery.UnitTest.Application.Handlers.CustomerHandlers
+{
+    public class GetCustomerByIdQueryHandlerTests
+    {
+        private readonly Mock<ICustomerRepository> _customerRepository;
+        private readonly GetCustomerByIdQuery _getCustomerByIdQuery;
+
+        public GetCustomerByIdQueryHandlerTests()
+        {
+            _customerRepository = new Mock<ICustomerRepository>();
+            _getCustomerByIdQuery = new GetCustomerByIdQuery(Guid.Empty);
+        }
+
+        [Fact]
+        public void GetCustomerByIdQueryHandler_Should_Return_Null_When_CustomerDoesNotExist()
+        {
+            //Arrange
+            Customer? ret = null;
+            _customerRepository.Setup(x => x.GetCustomerById(It.IsAny<Guid>()))
+                               .ReturnsAsync(ret);
+
+            //Act
+            var getCustomerByIdQueryHandler = new GetCustomerByIdQueryHandler(_customerRepository.Object);
+            var response = getCustomerByIdQueryHandler!.Handle(_getCustomerByIdQuery, default);
+
+            //Assert
+            response!.Result.Should().BeNull();
+        }
+    }
+}
