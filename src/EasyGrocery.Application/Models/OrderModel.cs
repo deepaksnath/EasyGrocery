0e45d27339d183414738ae2e495ddd93d05e81c4
deepaksnath <deepak.s.nath@gmail.com>
2025-02-20 20:13:59 +0530

    Order model updated.

--- src/EasyGrocery.Application/Models/OrderModel.cs
+++ src/EasyGrocery.Application/Models/OrderModel.cs
@@ -1,10 +1,13 @@
-﻿namespace EasyGrocery.Application.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EasyGrocery.Application.Models
 {
     public class OrderModel 
     {
         public Guid Id { get; set; }
         public Guid CustomerId { get; set; }
-        public DateTime OrderDate { get; set; } = DateTime.Now;
-        public decimal TotalAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }       
+        public bool IsLoyaltyMembershipAdded { get; set; }
     }
 }
